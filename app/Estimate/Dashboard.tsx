d8e35d70668404973aacf2c0ab7fd2a15ea0dd07
savagegraphics <olawaleraji188@gmail.com>
2023-11-23 17:35:46 -0800

    first commit

--- app/Estimate/Dashboard.tsx
+++ app/Estimate/Dashboard.tsx
@@ -1,14 +1,13 @@
 'use client;'
 import React from 'react'
 import Estimate from '@/components/component/estimate/estimatee'
-import Dashboard from './Dashboard'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
     <div className='overflow-auto'>
-      <Dashboard />
+      <Estimate />
     </div>
   )
 }
