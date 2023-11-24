95428f07730861f5406e4539c5730fb49ed07d07
savagegraphics <olawaleraji188@gmail.com>
2023-11-23 16:41:33 -0800

    first commit

--- app/Estimate/page.tsx
+++ app/Estimate/page.tsx
@@ -1,9 +1,15 @@
+'use client;'
 import React from 'react'
+import Estimate from '@/components/component/estimate/estimatee'
 
 type Props = {}
 
 const page = (props: Props) => {
-  return <div>page</div>
+  return (
+    <div className='overflow-auto'>
+      <Estimate />
+    </div>
+  )
 }
 
 export default page
