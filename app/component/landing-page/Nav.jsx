60bf538d33bc4f2982cfa411b9cb1cf322d9ea0c
savagegraphics <olawaleraji188@gmail.com>
2023-12-22 07:20:19 -0800

    my-changes

--- app/component/landing-page/Nav.jsx
+++ app/component/landing-page/Nav.jsx
@@ -60,7 +60,7 @@ const Nav = () => {
           </ul>
           <Link
             className='rounded-lg text-white text-sm md:text-md bg-[#0F2B48] hover:bg-[#0F2B48]  hover:border-white hover:border-1 px-4 py-2 cursor-pointer text-center'
-            href='/auth/AuthNotification'
+            href='../Auth'
           >
             Get Started
           </Link>
